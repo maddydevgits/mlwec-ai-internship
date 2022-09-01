6225091b18fae60a1a0ed25d935fef326937442e
MS <72651078+maddydevgits@users.noreply.github.com>
2022-09-01 15:35:59 +0530

    Create subscriber.py

---
+++ project2/subscriber.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+sub=mqtt.Client()
+broker='broker.hivemq.com'
+port=1883
+
+sub.connect('broker',port)
+print('Broker Connected')
+sub.subscribe('mlew/ai')
+
+def notification(sub,userdata,msg):
+    print(msg.payload)
+
+sub.on_message=notification
+sub.loop_forever()
